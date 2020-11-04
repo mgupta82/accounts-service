7f7c14ded245234e70a66c979c949110d142b3b4
mgupta82 <mukeshsgupta@gmail.com>
2020-11-03 23:21:26 -0500

    Add assertions

--- src/test/java/com/anz/accounts/controller/AccountsControllerTest.java
+++ src/test/java/com/anz/accounts/controller/AccountsControllerTest.java
@@ -1,52 +1,71 @@
 package com.anz.accounts.controller;
 
-import com.anz.accounts.dto.ErrorResponse;
-import com.anz.accounts.repository.entity.Account;
-import com.anz.accounts.repository.entity.Transaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.util.AssertionErrors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Sql(scripts= {"/scripts/test-accounts.sql"})
 public class AccountsControllerTest {
 
     @Autowired
-    TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
-    public void testGetAccounts() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity request = new HttpEntity<>(null);
-        ResponseEntity<Account[]> responseEntity = restTemplate.exchange("/api/accounts/testuser", HttpMethod.GET,request, Account[].class);
-        AssertionErrors.assertEquals("Invalid Http Status",HttpStatus.OK,responseEntity.getStatusCode());
-        AssertionErrors.assertEquals("Incorrect search result",2,responseEntity.getBody().length);
+    public void testGetAccounts() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/accounts/testuser"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].accountNumber").value("585309209"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].userId").value("testuser"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].accountName").value("SGSavings726"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].accountType").value("Savings"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].currency").value("AUD"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].balanceDate").value("2018-11-08"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].availableBalance").value("84327.51"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].accountNumber").value("485309209"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].userId").value("testuser"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].accountName").value("SGSavings726"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].accountType").value("Savings"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].currency").value("AUD"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].balanceDate").value("2018-11-08"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[1].availableBalance").value("84327.51"));
+    }
+
+   @Test
+    public void testGetTransactions() throws Exception {
+       mockMvc.perform(MockMvcRequestBuilders.get("/api/transactions/585309209"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[0].transId").value("12345"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[0].accountNumber").value("585309209"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[0].valueDate").value("2012-01-12"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[0].currency").value("SGD"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[0].amount").value("9540.98"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].transId").value("22345"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].accountNumber").value("585309209"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].valueDate").value("2012-01-12"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].currency").value("SGD"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].amount").value("-234.56"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[1].transNarrative").value("moving out"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].transId").value("32345"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].accountNumber").value("585309209"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].valueDate").value("2012-01-12"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].currency").value("SGD"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].amount").value("54.56"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.[2].transNarrative").value("moving in"));
     }
 
     @Test
-    public void testGetTransactions() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity request = new HttpEntity<>(null);
-        ResponseEntity<Transaction[]> responseEntity = restTemplate.exchange("/api/transactions/585309209", HttpMethod.GET,request, Transaction[].class);
-        AssertionErrors.assertEquals("Invalid Http Status",HttpStatus.OK,responseEntity.getStatusCode());
-        AssertionErrors.assertEquals("Incorrect search result",3,responseEntity.getBody().length);
-    }
-
-    @Test
-    public void testErrorScenario() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.set("content-type", MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity request = new HttpEntity<>(null);
-        ResponseEntity<ErrorResponse> responseEntity = restTemplate.exchange("/account/testuser", HttpMethod.POST,request, ErrorResponse.class);
-        AssertionErrors.assertEquals("Invalid Http Status",HttpStatus.NOT_FOUND,responseEntity.getStatusCode());
+    public void testErrorScenario() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/account/testuser"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
 }
